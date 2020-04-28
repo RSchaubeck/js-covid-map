8243ef67c7cc5f3d79f9bcfc20350a586b106a85
Richard Schaubeck <richard.schaubeck@gmail.com>
2020-04-28 17:11:24 -0400

    set bounds for map, ad populated dropdown with countries from api

---
+++ covid_stats.js
@@ -0,0 +1,19 @@
+const xhr = new XMLHttpRequest();
+
+xhr.open('GET', 'https://covid-api.com/api/regions');
+xhr.send();
+
+document.addEventListener("DOMContentLoaded", () => {
+    xhr.onload = function () {
+        const data = JSON.parse(xhr.responseText);
+        let countries = document.getElementById('country');
+        let country;
+        for (let i = 0; i < data["data"].length; i++) {
+            country = document.createElement('option');
+            country.text = data["data"][i].name;
+            country.text = data["data"][i].name;
+            country.value = data.data[i].iso;
+            countries.appendChild(country);
+        }
+    }
+});

769edd2cbc3bad764354b3d1d9403fa64ac71dfe
Richard Schaubeck <richard.schaubeck@gmail.com>
2020-04-27 22:14:06 -0400

    seperated concerns and added sidebar

---
+++ map.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", () => {
+    var map = L.map('map').setView([15, 10], 2);
+
+
+    L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png', {
+        attribution: '&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
+    }).addTo(map);
+});

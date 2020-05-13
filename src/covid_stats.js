16ea7b8f1b6265e99a4a24305b5d9d9bedd199ae
Richard Schaubeck <richard.schaubeck@gmail.com>
2020-05-12 21:48:57 -0400

    circles get redrawn based on selected stat

--- src/covid_stats.js
+++ src/covid_stats.js
@@ -6,14 +6,61 @@ const map = L.map('map')
     .setView([0, 0], 2)
     .setMaxBounds(boundary);
 
-document.addEventListener("DOMContentLoaded", () => {
-    // const corner1 = L.latLng(-60, -180);
-    // const corner2 = L.latLng(70, 180);
-    // const boundary = L.latLngBounds(corner1, corner2);
+async function getCountryinfo() {
+    let countries = document.getElementById('country');
+    let covidStats = document.getElementById('covid-stats');
+    const xhr = new XMLHttpRequest();
+    return new Promise(function (resolve, reject) {
+        xhr.onload = function () {
+            if (xhr.status >= 300) {
+                reject(xhr.status)
+            } else {
+                let stat = covidStats.options[covidStats.selectedIndex].text;
+                let divisor;
+                switch (stat) {
+                    case "Confirmed Cases":
+                        stat = "cases";
+                        divisor = 10000;
+                        break;
+                    case "Deaths":
+                        stat = "deaths";
+                        divisor = 1000;
+                        break;
+                    case "Recovered":
+                        stat = "recovered";
+                        divisor = 2000;
+                        break;
+                    case "Cases Per Million":
+                        stat = "casesPerOneMillion";
+                        divisor = 200;
+                        break;
+                    case "Deaths Per Million":
+                        stat = "deathsPerOneMillion";
+                        divisor = 30;
+                        break;
+                    default:
+                        break;
+                }
+                const data = JSON.parse(xhr.responseText);
+                let country;
+                let coords = [];
+                for (let i = 0; i < data.length; i++) {
+                    country = document.createElement('option');
+                    country.text = data[i].country;
+                    country.value = data[i].countryInfo.iso3;
+                    countries.appendChild(country);
+                    let coord = { long: data[i].countryInfo.long, lat: data[i].countryInfo.lat, radius: (data[i][stat] / divisor) };
+                    coords.push(coord);
+                }
+                resolve(coords);
+            }
+        }
+        xhr.open('GET', 'https://corona.lmao.ninja/v2/countries?yesterday&sort');
+        xhr.send();
+    });
+}
 
-    // const map = L.map('map')
-    //     .setView([0, 0], 2)
-    //     .setMaxBounds(boundary);
+document.addEventListener("DOMContentLoaded", () => {
 
     L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png', {
         attribution: '&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors',
@@ -24,34 +71,6 @@ document.addEventListener("DOMContentLoaded", () => {
 
 
     L.svg().addTo(map);
-    
-    let countries = document.getElementById('country');
-
-    async function getCountryinfo() {
-        const xhr = new XMLHttpRequest();
-        return new Promise(function (resolve, reject) {
-            xhr.onload = function () {
-                if (xhr.status >= 300) {
-                    reject(xhr.status)
-                } else {
-                    const data = JSON.parse(xhr.responseText);
-                    let country;
-                    let coords = [];
-                    for (let i = 0; i < data.length; i++) {
-                        country = document.createElement('option');
-                        country.text = data[i].country;
-                        country.value = data[i].countryInfo.iso3;
-                        countries.appendChild(country);
-                        let coord = { long: data[i].countryInfo.long, lat: data[i].countryInfo.lat, radius: (data[i].cases/10000)};
-                        coords.push(coord);
-                    }
-                    resolve(coords);
-                }
-            }
-            xhr.open('GET', 'https://corona.lmao.ninja/v2/countries?yesterday&sort');
-            xhr.send();
-        });
-    }
 
     async function drawCircles() {
         try {
@@ -92,15 +111,36 @@ function changeCountry() {
     const req = new XMLHttpRequest();
     req.open('GET', `https://corona.lmao.ninja/v2/countries/${iso}`);
     req.send();
-    let coord;
     if (iso === "all countries") { 
         map.setView([0, 0], 2)
     } else {
         req.onload = function () {
-            data = JSON.parse(req.responseText);
+            let data = JSON.parse(req.responseText);
             // debugger
-            coord = [data.countryInfo.lat, data.countryInfo.long];
+            let coord = [data.countryInfo.lat, data.countryInfo.long];
             map.setView(coord, 5);
         }
+    }
+}
+
+async function drawCircles() {
+    d3.selectAll("svg > *").remove();
+    try {
+        let coords = await getCountryinfo();
+        d3.select("#map")
+            .select("svg")
+            .selectAll("myCircles")
+            .data(coords)
+            .enter()
+            .append("circle")
+            .attr("cx", function (d) { return map.latLngToLayerPoint([d.lat, d.long]).x })
+            .attr("cy", function (d) { return map.latLngToLayerPoint([d.lat, d.long]).y })
+            .attr("r", function (d) { return d.radius })
+            .style("fill", "red")
+            .attr("stroke", "red")
+            .attr("stroke-width", 1)
+            .attr("fill-opacity", .1)
+    } catch (err) {
+        console.log(err);
     }
 }

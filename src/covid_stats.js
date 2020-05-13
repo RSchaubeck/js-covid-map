9dad796bc74aefba28309aedec763d58dd082dab
Richard Schaubeck <richard.schaubeck@gmail.com>
2020-05-13 12:11:50 -0400

    added map tiles to index page

--- src/covid_stats.js
+++ src/covid_stats.js
@@ -64,12 +64,12 @@ async function getCountryinfo() {
 
 document.addEventListener("DOMContentLoaded", () => {
 
-    L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png', {
-        attribution: '&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors',
-        maxBoundsViscosity: 1.0,
-        minZoom: 2,
-        maxZoom: 8
-    }).addTo(map);
+    // L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png', {
+    //     attribution: '&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors',
+    //     maxBoundsViscosity: 1.0,
+    //     minZoom: 2,
+    //     maxZoom: 8
+    // }).addTo(map);
 
 
     L.svg().addTo(map);

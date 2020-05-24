9753a04226f625c2468f8caac707a681b68b4145
Richard Schaubeck <richard.schaubeck@gmail.com>
2020-05-24 16:26:03 -0400

    selected stat now displays number on hover of country

--- src/covid_stats.js
+++ src/covid_stats.js
@@ -7,27 +7,22 @@ async function getCountryinfo() {
             if (xhr.status >= 300) {
                 reject(xhr.status)
             } else {
-                let stat = covidStats.options[covidStats.selectedIndex].text;
+                let stat = covidStats.options[covidStats.selectedIndex].value;
                 let divisor;
                 switch (stat) {
-                    case "Confirmed Cases":
-                        stat = "cases";
+                    case "cases":
                         divisor = 10000;
                         break;
-                    case "Deaths":
-                        stat = "deaths";
+                    case "deaths":
                         divisor = 1000;
                         break;
-                    case "Recovered":
-                        stat = "recovered";
+                    case "recovered":
                         divisor = 2000;
                         break;
-                    case "Cases Per Million":
-                        stat = "casesPerOneMillion";
+                    case "casesPerOneMillion":
                         divisor = 200;
                         break;
-                    case "Deaths Per Million":
-                        stat = "deathsPerOneMillion";
+                    case "deathsPerOneMillion":
                         divisor = 30;
                         break;
                     default:
@@ -42,6 +37,7 @@ async function getCountryinfo() {
                     country.value = data[i].countryInfo.iso3;
                     countries.appendChild(country);
                     let coord = { 
+                        name: data[i].country,
                         long: data[i].countryInfo.long, 
                         lat: data[i].countryInfo.lat, 
                         radius: (data[i][stat] / divisor), 
@@ -61,42 +57,63 @@ async function getCountryinfo() {
     });
 }
 
+let popup = d3.select("body").append("div")
+    .attr("class", "popup")
+    .style("opacity", 0);
+
+async function drawCircles() {
+    let covidStats = document.getElementById('covid-stats');
+    let stat = covidStats.options[covidStats.selectedIndex].value
+    d3.selectAll("svg > *").remove();
+    try {
+        let coords = await getCountryinfo();
+        d3.select("#map")
+            .select("svg")
+            .selectAll("myCircles")
+            .data(coords)
+            .enter()
+            .append("circle")
+            .attr("pointer-events", "visible")
+            .attr("cx", function (d) { return map.latLngToLayerPoint([d.lat, d.long]).x })
+            .attr("cy", function (d) { return map.latLngToLayerPoint([d.lat, d.long]).y })
+            .attr("r", function (d) { return d.radius })
+            .style("fill", "red")
+            .attr("stroke", "red")
+            .attr("stroke-width", 1)
+            .attr("fill-opacity", .1)
+            .on("mouseover", function(d) {
+                d3.select(this).transition()
+                    .duration('100')
+                    .style("fill", "midnightblue")
+                    .attr("fill-opacity", .1)
+                    .attr("r", function (d) { return d.radius * 1.2 })
+                popup.transition()
+                    .duration(100)
+                    .style("opacity", 1);
+                popup.html(d.name + "<br>" + stat + ": " + d[stat])
+                    .style("left", (d3.event.pageX) + "px")
+                    .style("top", (d3.event.pageY) + "px")
+                    
+            })
+            .on("mouseout", function() {
+                d3.select(this)
+                    .attr("r", function (d) { return d.radius })
+                    .style("fill", "red")
+                    .attr("stroke", "red")
+                    .attr("stroke-width", 1)
+                    .attr("fill-opacity", .1)
+                popup.transition()
+                    .duration('100')
+                    .style("opacity", 0)
+            })
+    } catch (err) {
+        console.log(err);
+    }
+}
+
 document.addEventListener("DOMContentLoaded", () => {
 
     L.svg().addTo(map);
-
-    async function drawCircles() {
-        try {
-            let coords = await getCountryinfo();
-            d3.select("#map")
-                .select("svg")
-                .selectAll("myCircles")
-                .data(coords)
-                .enter()
-                .append("circle")
-                .attr("cx", function (d) { return map.latLngToLayerPoint([d.lat, d.long]).x })
-                .attr("cy", function (d) { return map.latLngToLayerPoint([d.lat, d.long]).y })
-                .attr("r", function (d) { return d.radius })
-                .style("fill", "red")
-                .attr("stroke", "red")
-                .attr("stroke-width", 1)
-                .attr("fill-opacity", .1)
-                .on("mouseover", function(d) {
-                    d3.select(this.parentNode)
-                        .select("circle")
-                        // .data(coords)
-                        // .enter()
-                        // .attr("r", function (d) { return d.radius * 2 })
-                        .style("fill", "darkred")
-                        .attr("stroke", "darkred")
-                        .attr("stroke-width", 3)
-                        .attr("fill-opacity", .5)
-                })
-                // .on("mouseout", handleMouseOut)
-        } catch (err) {
-            console.log(err);
-        }
-    }
 
     drawCircles();
 
@@ -116,31 +133,13 @@ function changeCountry() {
     const req = new XMLHttpRequest();
     req.open('GET', `https://corona.lmao.ninja/v2/countries/${iso}`);
     req.send();
-    if (iso === "all countries") { 
+    if (iso === "all countries") {
         map.setView([0, 0], 2)
     } else {
         req.onload = function () {
             let data = JSON.parse(req.responseText);
-            // debugger
             let coord = [data.countryInfo.lat, data.countryInfo.long];
             map.setView(coord, 5);
         }
     }
 }
-
-// async function handleMouseOver() {
-//     try {
-//         let coords = await getCountryinfo();
-//         d3.select(this)
-//             .select("svg")
-//             .data(coords)
-//             .enter()
-//             .attr("r", function (d) { return d.radius * 2 })
-//             .style("fill", "darkred")
-//             .attr("stroke", "darkred")
-//             .attr("stroke-width", 1)
-//             .attr("fill-opacity", .1)
-//     } catch (err) {
-//         console.log(err);
-//     }
-// }
